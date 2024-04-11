50edf0ded2c1d4fbc68d1afd7a1831b43a4d2902
StexRoles <navarro456123@gmail.com>
2024-04-11 18:05:41 -0500

    añadiendo las categorias

--- src/components/GenresInDb.jsx
+++ src/components/GenresInDb.jsx
@@ -1,58 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 function GenresInDb() {
-    return (
-        <>
-            {/* Genres in DB */}
-            <div className="col-lg-6 mb-4">
-                <div className="card shadow mb-4">
-                    <div className="card-header py-3">
-                        <h5 className="m-0 font-weight-bold text-gray-800">Categorias</h5>
-                    </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Laptops
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Oficina
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Estudiantes
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Accesosrios
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Gamer
-                                    </div>
-                                </div>
-                            </div>
+    const [products, setProducts] = useState(null);
 
-                        </div>
+    useEffect(() => {
+        fetch('/api/product/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    return (
+        <div className="col-lg-6 mb-4">
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h5 className="m-0 font-weight-bold text-gray-800">Categorias</h5>
+                </div>
+                <div className="card-body">
+                    <div className="row">
+                        {products && products.countByCategory.map((category, index) => (
+                            <div key={index} className="col-lg-6 mb-4">
+                                <div className="card bg-dark text-white shadow">
+                                    <div className="card-body">
+                                        {category.category}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default GenresInDb
+export default GenresInDb;
